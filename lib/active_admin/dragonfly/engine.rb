9c7cbf9e6f9ceff6a3c37c0abc89ef604962440a
Stefano Verna <stefano.verna@welaika.com>
2012-10-24 14:27:04 +0200

    Fix

--- lib/active_admin/dragonfly/engine.rb
+++ lib/active_admin/dragonfly/engine.rb
@@ -2,9 +2,16 @@ require 'dragonfly'
 
 module ActiveAdmin
   module Dragonfly
-    class Engine < ::Rails::Engine
 
-      initializer "Railsyard precompile hook" do |app|
+    class Engine < ::Rails::Engine
+      initializer "Railsyard precompile hook", group: :all do |app|
+        # check if someone already initialized Dragonfly for Rails
+        unless ActiveRecord::Base.methods.include? :image_accessor
+          require 'dragonfly/rails/images'
+        end
+        # in any case we add the Dragonfly[:images] app as middleware
+        app.config.middleware.insert 1, 'Dragonfly::Middleware', :images
+
         app.config.assets.precompile += [
           "active_admin/active_admin_dragonfly.js",
           "active_admin/active_admin_dragonfly.css"
@@ -14,7 +21,7 @@ module ActiveAdmin
       config.to_prepare do
         ActiveAdmin.application.register_stylesheet "active_admin/active_admin_dragonfly.css", :media => :screen
       end
-
     end
+
   end
 end
