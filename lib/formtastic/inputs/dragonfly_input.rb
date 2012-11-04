fb84f644e279b58926f6f0574d1c2790a5cfa58d
Stefano Verna <stefano.verna@welaika.com>
2012-11-04 16:34:14 +0100

    Fixes

--- lib/formtastic/inputs/dragonfly_input.rb
+++ lib/formtastic/inputs/dragonfly_input.rb
@@ -67,21 +67,32 @@ module Formtastic
       end
 
       def fragment_preview_html
-
-        preview = if object.send("#{method}_uid")
-          original_url = object.send(method).url
-          if object.send("#{method}_name").match /\.(png|gif|jpe?g)$/
+        image = object.send(method)
+        if image.present?
+          if image.image?
+            original_url = object.send(method).url
             preview_size = input_html_options[:preview_size] || "72x72#"
             preview_url = object.send(method).thumb(preview_size).url
-            template.link_to(template.image_tag(preview_url), original_url)
+            fragment_label_html(:preview) << template.link_to(template.image_tag(preview_url), original_url)
           else
-            template.link_to object.send("#{method}_name"), original_url
+            fragment_download_html
           end
         else
-          "<div class='no-image'>#{I18n.t("dragonfly.no_image")}</div>".html_safe
+          fragment_label_html(:preview) << "<div class='no-image'>#{I18n.t("dragonfly.no_image")}</div>".html_safe
         end
+      end
 
-        fragment_label_html(:preview) << preview
+      def fragment_download_html
+        file = object.send(method)
+        download = if file.present?
+          original_url = file.url
+          name = object.send("#{method}_name") rescue "Download"
+          name = name.blank? ? "Download" : name
+          template.link_to name, original_url
+        else
+          "<span class='no-file'>#{I18n.t("dragonfly.no_file")}</div>".html_safe
+        end
+        fragment_label_html(:download) << download
       end
 
       def fragment_url_html
